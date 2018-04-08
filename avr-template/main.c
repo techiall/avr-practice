1052afb4cdf0e4eb474033b63454415513db3cdf
techial <techial@163.com>
2018-04-08 18:37:33 +0800

    Update test-2

--- avr-template/main.c
+++ avr-template/main.c
@@ -5,81 +5,52 @@ imakew -f FIRST.mak
 #include <macros.h>
 
 const unsigned char led[] = {
-        0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,
-        0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71
+	0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,
+	0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71
 };
+
+void delay(unsigned int time)
+{
+	while (time--) {
+		int i = 0;
+		for (i = 0; i < 100; i++);
+	}
+}
+
+void set_number(int number, int digits)
+{
+	int i;
+	for (i = digits - 1; i >= 0; i--, number /= 10) {
+		PORTC = (1 << i);
+		PORTB = ~led[number % 10];
+		delay(4);
+	}
+}
 
 void init(void)
 {
-	CLI();
-	PORTA = 0x01;
-	DDRA  = 0x01;
-	
-	PORTB = 0x03;
-	DDRB  = 0x03;
-	
-	PORTD = 0xF0;
-	DDRD  = 0xF0;
+	DDRB = 0xFF;
+	PORTB = 0xFF;
 
-	TCCR0 = 0x00;
-	TCNT0 = 0x83;
-	OCR0  = 0x7D;
-	TCCR0 = 0x0B;
+	DDRC = 0x00;
+	PORTC = 0x00;
 
-	MCUCR = 0x00;
-	GICR  = 0x00;
-	TIMSK = 0x02; 
-	SEI();
+	DDRD &= ~(1 << 3);
+	PORTD |= (1 << 3);
 }
 
-unsigned int i = 0;
-unsigned char flag = 0, light = 0;
-
-#pragma interrupt_handler timer0_comp_isr:iv_TIM0_COMP
-void timer0_comp_isr(void)
+int main(void)
 {
-	i = (i + 1) % 101;
-	flag = i == 100 ? 1 : flag;
-	light = i % 2 ? light : 1;
-}
-
-void send_data(unsigned char data)
-{
-	int j = 0;
-	for (j = 7; j >= 0; j--) {
-		PORTA = data & (1 << j) ? PORTA | (1 << 0) : PORTA & ~(1 << 0);
-		PORTB |= (1 << 1);
-		PORTB &= ~(1 << 1);
-	}
-	PORTB |= (1 << 0);
-	PORTB &= ~(1 << 0);
-}
-
-void main(void)
-{
-	unsigned int num = 0;
-	unsigned int time[4] = {0};
-	char k = 0;
-	int tmp = 0;
-	
+	int number = 0;
 	init();
-	while(1) {
-		if(flag) {
-			flag = 0;
-			if (++num % 100 >= 60) {
-				num = (num + 100) % 10000;
-				num -= num % 100;
+	while (1) {
+		set_number(number, 4);
+		if (!(PIND & (1 << 3)))  {
+			while (!(PIND & (1 << 3))) {
+				set_number(number, 4);	
 			}
-			for (tmp = num, k = 0; k < 4; k++, tmp /= 10)
-				time[4 - 1 - k] = tmp % 10;
+			number++;
 		}
-		for(k = 0; k < 4; k++) {
-			send_data(~led[time[k]]);
-			PORTD &= ~(1 << (7 - k));
-			while(!light);
-			light = 0;
-			PORTD |= (1 << (7 - k));
-		}
-
 	}
+	return 0;
 }
