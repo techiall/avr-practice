f75138612c8f30fb68a0be66fb73a6a521613af7
techial <techial@163.com>
2018-05-04 18:11:37 +0800

    modify template

--- experiment-board-template/init_avr.h
+++ experiment-board-template/init_avr.h
@@ -1,22 +1,50 @@
 #ifndef _INIT_AVR_H_
 #define _INIT_AVR_H_
 
+void port_init(void)
+{
+	PORTA = 0x00;
+	DDRA  = 0x70;
+	PORTB = 0x00;
+	DDRB  = 0xB0;
+	PORTC = 0x00;
+	DDRC  = 0x00;
+	PORTD = 0x00;
+	DDRD  = 0x00;
+}
+
+void uart0_init(void)
+{
+	UCSRB = 0x00;
+	UCSRA = 0x00;
+	UCSRC = BIT(URSEL) | 0x06;
+	UBRRL = 0x2F;
+	UBRRH = 0x00;
+	UCSRB = 0x18;
+}
+
+void spi_init(void)
+{
+	SPCR = 0x50;
+	SPSR = 0x00;
+}
+
+void time_init(void)
+{
+	TCCR0 = 0x00;
+	TCNT0 = 0xC2;
+	OCR0  = 0x3E;
+	TCCR0 = 0x0B;
+}
+
 void init(void)
 {
 	CLI();
-	PORTA = 0x00;
-	DDRA  = 0x01;
 
-	PORTB = 0x00;
-	DDRB  = 0x03;
-
-	PORTD = 0xff;
-	DDRD  = 0xF0;
-
-	TCCR0 = 0x00;
-	TCNT0 = 0x83;
-	OCR0  = 0x7D;
-	TCCR0 = 0x0B;
+	port_init();
+	spi_init();
+	uart0_init();
+	time_init();
 
 	MCUCR = 0x00;
 	GICR  = 0x00;
